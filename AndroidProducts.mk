d0ef1072af2506cdbd44e661a69b65e18139a27f
plueygoofy <nileaiden410@gmail.com>
2025-10-18 13:58:09 +0530

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_boot.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_boot-user \
+    omni_boot-userdebug \
+    omni_boot-eng
+
---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+add_lunch_combo omni_boot-user
+add_lunch_combo omni_boot-userdebug
+add_lunch_combo omni_boot-eng
+
